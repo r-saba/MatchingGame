53111aabb4723e41670b79f4df002c28bedffac6
Ragith Sabapathipillai <ragith.coolkid@gmail.com>
2018-11-14 06:32:20 -0500

    Added method to assign icons to all squares

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,9 +12,38 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
+        List<string> icons = new List<string>()
+        {
+                "!", "!", "N", "N", ",", ",", "k", "k",
+                "b", "b", "v", "v", "w", "w", "z", "z"
+        };
+
         public Form1()
         {
             InitializeComponent();
+            AssignIconsToSquares();
+
+        }
+
+        private void AssignIconsToSquares()
+        {
+            // The TableLayoutPanel has 16 labels,
+            // and the icon list has 16 icons,
+            // so an icon is pulled at random from the list
+            // and added to each label
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    // iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
         }
     }
 }
